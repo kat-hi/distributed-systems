bcdb17e7b2a8c669f0ae4dc1bd609b19a06f3184
kat-ho <s77518@beuth-hochschule.de>
2019-10-20 23:37:41 +0200

    connection established

--- time_request/rust/src/main.rs
+++ time_request/rust/src/main.rs
@@ -1,12 +1,17 @@
 use std::net::{TcpStream};
-use std::io::{Read, Write};
-use std::str::from_utf8;
+// use std::io::{Read, Write};
+// use std::str::from_utf8;
 
-fn main() {
-    match TcpStream::connect("dbl44.beuth-hochschule.de:21"){
-        Ok(mut Stream) => {
-            println!("Connection established:")
-        }
+fn connect () {
+    if let Ok(stream) = TcpStream::connect("dbl44.beuth-hochschule.de:21") {
+        println!("Connection established:");
+    } else {
+        println!("Couldn't connect to server...");
     }
 }
 
+fn main () {
+    let hostname = "dbl44.beuth-hochschule.de";
+    let port = "21";
+    connect();
+}
--- time_request/rust/target/debug/deps/rust-aa8032e28f8090ac.d
+++
@@ -1,5 +0,0 @@
-/home/kat/Documents/studium/current/disSys/tasks/time_request/rust/target/debug/deps/rust-aa8032e28f8090ac: src/main.rs
-
-/home/kat/Documents/studium/current/disSys/tasks/time_request/rust/target/debug/deps/rust-aa8032e28f8090ac.d: src/main.rs
-
-src/main.rs:

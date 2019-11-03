5c4dad05e2e78fe61b9a998061a213db103a88d6
kat-ho <s77518@beuth-hochschule.de>
2019-11-04 00:15:05 +0100

    sniffer watches network traffic

--- personal_chat/sniffer.py
+++ personal_chat/sniffer.py
@@ -1,13 +1,12 @@
 import re
 import socket
-# this script fetches all ip adressess in current network traffic
+# this script fetches all ip addresses in current network traffic
 #
-# the plan: ssh portforwarding: compute.beuth -> dbl44
+# the plan: ssh port forwarding: compute.beuth -> dbl44.beuth
 # execute this script and watch the traffic. do regex and filter usernames?
 # I wasn't allowed to watch network traffic on compute@beuth-hochschule.de
 #
 s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
-
 while True:
 	response = str(s.recvfrom(65565))
 	try:

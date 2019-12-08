7a33932050e0a4f316e461f1cdb46be4c6c3a162
kat-ho <s77518@beuth-hochschule.de>
2019-12-08 20:54:36 +0100

    remove outsourcing

--- dslp_server/DslpFactory.py
+++
@@ -1,9 +0,0 @@
-from twisted.internet.protocol import Factory
-from Serversession import User
-
-class DslpFactory(Factory):
-	def buildProtocol(self, addr):
-		user = User(addr.host, addr.port)
-		print('Connection established')
-		from Server import Dslp
-		return Dslp(user)
